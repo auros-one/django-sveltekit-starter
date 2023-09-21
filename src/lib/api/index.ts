ff6789ff24f4e5044ccacfc9d184d6c84a5570f1
Stijn-B <stijn1c@gmail.com>
2023-09-21 18:15:44 +0200

    Typed fetch apiClient (openapi-fetch) (#5)
    * setup type generation script
    
    * api request are proxied through /api
    
    * export client instead of separate methods

---
+++ src/lib/api/index.ts
@@ -0,0 +1,13 @@
+/**
+ * We using openapi-fetch to generate typescript client for our backend API.
+ * https://openapi-ts.pages.dev/openapi-fetch/
+ * 
+ * Update the types by syncing the schema from backend:
+ * > npm run sync-types
+ */
+import createClient from 'openapi-fetch';
+import type { paths } from './backend-api-schema.d.ts';
+
+const apiClient = createClient<paths>({ baseUrl: '/api' }); // api request are proxied through /api
+
+export { apiClient };
