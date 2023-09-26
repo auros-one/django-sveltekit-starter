3b1c9bd82908620b064d2ad4fe614597d8bca79a
Stijn <stijn1c@gmail.com>
2023-09-26 09:08:17 +0200

    datetime utils

--- project/utils/tests/test_views.py
+++ project/utils/tests/test_views.py
@@ -1,22 +1,8 @@
 import pytest
-from hypothesis import given
-from hypothesis.extra.django import from_field
 from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
 from rest_framework.test import APIClient
 
-from django import forms
 from project.accounts.models import User
-
-from .fields import SlugField
-
-
-@given(slug=from_field(SlugField(blank=True)))
-def test_slugfield_form(slug):
-    class SlugFieldForm(forms.Form):
-        slug = SlugField(blank=True).formfield()
-
-    form = SlugFieldForm(data={"slug": slug})
-    assert form.is_valid()
 
 
 def test_healthcheck(api_client: APIClient) -> None:
