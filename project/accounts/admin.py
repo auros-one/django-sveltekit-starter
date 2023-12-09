efe658df53a11069fdae20f40d3cbf8ca4e9554d
Stijn <stijn1c@gmail.com>
2023-12-09 18:41:26 +0100

    unregister redundant models from admin

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
 from django.core.exceptions import ValidationError
 
 from .models import User
@@ -103,3 +104,6 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     ]
+
+
+admin.site.unregister(Group)
--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,5 +1,11 @@
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+from allauth.socialaccount.models import SocialApp
+from allauth.socialaccount.models import SocialToken
+from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from rest_framework.authtoken.models import TokenProxy
 
 from project.accounts.views import ChangeEmailView
 
@@ -8,3 +14,11 @@ urlpatterns = [
     path("", include("dj_rest_auth.urls")),
     path("change-email/", ChangeEmailView.as_view(), name="change-email"),
 ]
+
+
+# We can't unregister these in the admin.py file because they won't be registered yet.
+admin.site.unregister(EmailAddress)
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(TokenProxy)
