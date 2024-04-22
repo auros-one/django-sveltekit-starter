87198813d91b806c324c12efe88b64e0d737de00
Stijn <stijn1c@gmail.com>
2024-04-23 00:12:35 +0200

    Remove BaseModel.deleted

--- project/utils/models.py
+++ project/utils/models.py
@@ -1,38 +1,13 @@
-import datetime
 import uuid
-from datetime import timezone
 
 from django.db import models
-
-
-class BaseModelManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(deleted=False)
-
-    def deleted(self):
-        return super().get_queryset().filter(deleted=True)
 
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-
-    objects: BaseModelManager = BaseModelManager()
 
     class Meta:
         ordering = ["-created"]
         abstract = True
-
-    def save(self, *args, **kwargs):
-        if self.deleted:
-            self.deleted_at = datetime.datetime.now(timezone.utc)
-        else:
-            self.deleted_at = None
-        super().save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        self.deleted = True
-        self.save()
