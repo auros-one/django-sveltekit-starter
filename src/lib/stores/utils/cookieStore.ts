81de9aba810caad893a467867589d17c4b876e65
Stijn <stijn1c@gmail.com>
2024-04-24 23:25:47 +0200

    fix cookieStore for Safari

--- src/lib/stores/utils/cookieStore.ts
+++ src/lib/stores/utils/cookieStore.ts
@@ -1,8 +1,9 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
+import Cookies from 'js-cookie';
 
 export function persisted<T>(key: string, initial: T | null): ReturnType<typeof writable<T>> {
-	const json = getCookie(key);
+	const json = Cookies.get(key);
 	const initialValue = json ? JSON.parse(json) : initial;
 	const store = writable<T>(initialValue);
 
@@ -13,25 +14,13 @@ export function persisted<T>(key: string, initial: T | null): ReturnType<typeof 
 	return store;
 }
 
-function getCookie(key: string): string | undefined {
-	if (browser) {
-		return document.cookie
-			.split('; ')
-			.find((row) => row.startsWith(`${key}=`))
-			?.split('=')[1];
-	}
-}
-
 function setCookie<T>(key: string, value: T | null): void {
 	if (browser) {
 		if (value === null) {
-			// Set the cookie to expire in the past, effectively deleting it
-			document.cookie = `${key}=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Strict; Secure; Path=/`;
+			Cookies.remove(key);
 		} else {
 			const json = JSON.stringify(value);
-			const expires = new Date();
-			expires.setFullYear(expires.getFullYear() + 1);
-			document.cookie = `${key}=${json}; Expires=${expires.toString()}; SameSite=Strict; Secure; Path=/`;
+			Cookies.set(key, json, { expires: 365 });
 		}
 	}
 }
