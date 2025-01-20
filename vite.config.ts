eefc8c17b46367f171f5f630ad73d6e1a3d1bd4d
Stijn-B <stijn1c@gmail.com>
2025-01-20 16:59:17 +0100

    fix vite issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN } from '$env/static/private';
+
 export default defineConfig({
 	build: {
 		sourcemap: true
 	},
 	plugins: [
 		sentryVitePlugin({
-			org: SENTRY_ORG,
-			project: SENTRY_PROJECT,
-			authToken: SENTRY_AUTH_TOKEN
+			org: process.env.SENTRY_ORG,
+			project: process.env.SENTRY_PROJECT,
+			authToken: process.env.SENTRY_AUTH_TOKEN
 		}),
 		sveltekit()
 	],
