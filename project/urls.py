0b26e6c52cb2a4588cc948cab4789f2be8a76daa
Stijn-B <stijn1c@gmail.com>
2025-01-20 11:36:46 +0100

    move backend url to /api prefix

--- project/urls.py
+++ project/urls.py
@@ -4,10 +4,10 @@ from django.urls import include, path
 from project.core.views import AuthCheck, HealthCheck
 
 urlpatterns = [
-    path("__debug__/", include("debug_toolbar.urls")),
-    path("admin/", admin.site.urls),
-    path("docs/", include("project.core.docs.urls")),
-    path("accounts/", include("project.accounts.urls")),
-    path("healthcheck/", HealthCheck.as_view(), name="healthcheck"),
-    path("authcheck/", AuthCheck.as_view(), name="authcheck"),
+    path("api/__debug__/", include("debug_toolbar.urls")),
+    path("api/admin/", admin.site.urls),
+    path("api/docs/", include("project.core.docs.urls")),
+    path("api/accounts/", include("project.accounts.urls")),
+    path("api/healthcheck/", HealthCheck.as_view(), name="healthcheck"),
+    path("api/authcheck/", AuthCheck.as_view(), name="authcheck"),
 ]
