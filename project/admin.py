172bbea7f29cd71e97942622ff461e46dc044bc9
Stijn-B <stijn1c@gmail.com>
2025-01-20 11:37:06 +0100

    upgrade CustomAdminSite

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,9 @@
-from django.conf import settings
 from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularRedocView
+
+from project.core.docs.utils import get_docs_title
+from project.core.utils.frontend import get_frontend_base_url
 
 # We try to get the site name from the database, but this won't work
 # on a fresh project that hasn't done any migrations yet.
@@ -22,7 +26,20 @@ class CustomAdminSite(admin.AdminSite):
     index_title = f"{site_name} administration"
 
     # The URL for the “View site” link at the top of each admin page.
-    if settings.ENVIRONMENT == "development":
-        site_url = f"http://{settings.FRONTEND_DOMAINS[0]}/"
-    else:  # pragma: no cover
-        site_url = f"https://{settings.FRONTEND_DOMAINS[0]}/"
+    site_url = get_frontend_base_url()
+
+    def get_urls(self):
+
+        urls = super().get_urls()
+        urls = [
+            path(
+                "docs/",
+                self.admin_view(
+                    SpectacularRedocView.as_view(
+                        url_name="schema", title=get_docs_title()
+                    )
+                ),
+                name="docs",
+            )
+        ] + urls
+        return urls
