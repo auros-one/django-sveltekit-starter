0c59d9015a9ab5d1190b539e54be1d6da3b0be0c
Stijn-B <stijn1c@gmail.com>
2025-06-10 16:36:09 +0200

    don't proxy refresh-token

--- frontend/src/routes/(auth)/refresh-token/+server.ts
+++ frontend/src/routes/(auth)/refresh-token/+server.ts
@@ -1,4 +1,5 @@
 import { error } from '@sveltejs/kit';
+import { PUBLIC_BASE_API_URL } from '$env/static/public';
 import type { RequestHandler } from './$types';
 
 export const GET = (async ({ cookies, fetch, url: _url }) => {
@@ -8,7 +9,7 @@ export const GET = (async ({ cookies, fetch, url: _url }) => {
 		if (refreshToken === undefined) error(401);
 
 		// get new JWT using the refresh token
-		const response = await fetch('/api/accounts/token/refresh/', {
+		const response = await fetch(PUBLIC_BASE_API_URL + '/api/accounts/token/refresh/', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
